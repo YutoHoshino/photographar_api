a6136be0e1c8f5b604fb315dc73cac187efb77ff
YutoHoshino <yutohoshino@Mac.local>
2022-02-08 01:11:52 +0900

    画像ファイルをJSで表示&削除できるように実装

--- frontend/src/components/Modal/PostModal.tsx
+++ frontend/src/components/Modal/PostModal.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useState } from "react";
 import styled from 'styled-components';
 
-import { DialogContent, Dialog, DialogTitle, TextField } from '@material-ui/core';
+import { DialogContent, Dialog, DialogTitle, TextField, Button, makeStyles, Theme, IconButton } from '@material-ui/core';
 
 //components
 import { PrimaryTextField } from 'components/TextField/PrimaryTextField';
@@ -23,9 +23,61 @@ const ButtonWrapper = styled.div`
   bottom: 10px;
 `
 
+const ImageWapper = styled.div`
+  padding: 15px;
+  height: 80px;
+  display: flex;
+`
+
+const ImagesArea = styled.div`
+  width: 100%;
+  padding: 0 10px;
+  display: flex;
+`
+
+const ImagesBox = styled.div`
+  position: relative;
+  width: 80px;
+  height: 80px;
+  border: 1px solid #f3f3f3;
+`
+
+const Image = styled.img`
+  object-fit: contain;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  margin: 0;
+`
+
+
+
+const useStyles = makeStyles((theme: Theme) => ({
+
+}))
+
 export const PostModal = (props: Props) => {
 
+  const classes = useStyles();
+
   const [text, setText] = useState<string>("")
+  const [images, setImages] = useState<File[]>([]);
+  const maxImagesUpload = 3;
+  const inputId = Math.random().toString(32).substring(2);
+
+  // 写真追加
+  const handleOnAddImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) return;
+    const img: File = e.target.files[0];
+    setImages([...images, img]);
+  };
+
+  // 写真削除
+  const handleOnRemoveImage = (index: number) => {
+    const newImages = [...images];
+    newImages.splice(index, 1);
+    setImages(newImages);
+  };
 
   return (
   <Dialog
@@ -37,6 +89,8 @@ export const PostModal = (props: Props) => {
     <DialogTitle>写真を投稿する</DialogTitle>
     <form>
       <DialogInner>
+
+
         <TextField
           rows={4}
           variant="outlined"
@@ -48,7 +102,42 @@ export const PostModal = (props: Props) => {
           onChange={e => setText(e.target.value)}
         />
 
-
+        <ImageWapper>
+          <label htmlFor={inputId}>
+            <Button
+              variant="contained"
+              disabled={images.length >= maxImagesUpload}
+              component="span"
+            >
+              ＋
+            </Button>
+            <input
+              id={inputId}
+              type="file"
+              multiple
+              accept="image/*,.png,.jpg,.jpeg,.gif"
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleOnAddImage(e)}
+              style={{ display: "none" }}
+            />
+          </label>
+          <ImagesArea>
+            {images.map((image, i) => (
+              <ImagesBox key={i} >
+                <Image src={URL.createObjectURL(image)}/>
+                <IconButton
+                  style={{
+                    top: -15,
+                    left: -10,
+                    color: "#aaa"
+                  }}
+                  onClick={() => handleOnRemoveImage(i)}>
+                    <div>×</div>
+                </IconButton>
+              </ImagesBox>
+            ))}
+          </ImagesArea>
+        </ImageWapper>
+ 
         <ButtonWrapper>
           <SubmitButton>送信</SubmitButton>
         </ButtonWrapper>
