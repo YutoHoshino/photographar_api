33e7e7bc8d19e4f18013124860275a550235b6a1
YutoHoshino <yutohoshino@Mac.local>
2022-03-01 17:19:09 +0900

    コンポーネント分割

---
+++ frontend/src/components/organisms/UserProfile/UserProfileItem.tsx
@@ -0,0 +1,93 @@
+import { useHistory } from "react-router-dom";
+import styled from "styled-components";
+
+//material
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import { Typography, CardContent, Button, CardActions } from "@material-ui/core";
+
+import SettingsIcon from '@material-ui/icons/Settings';
+
+// atom
+import { UserAveter } from "components/atoms/Avater/UserAvater";
+
+// interface
+import { UserData } from "interfaces/data/UserData";
+import { User } from "interfaces/get/User";
+
+const FlexBox = styled(Box)`
+  display: flex;
+`
+const ImageWapper = styled(Grid)`
+  padding: 10px;
+`
+const FlexCardContent = styled(CardContent)`
+  display: flex;
+`
+const EditButton = styled(Button)`
+  margin-left: 15px;
+`
+
+interface Props {
+  UserData: UserData
+  currentUser: User
+}
+
+export const UserProfileItem = (props: Props) => {
+
+  const history = useHistory();
+  
+  const { UserData, currentUser } = props
+  
+  return (
+    <FlexBox>
+
+      <ImageWapper>
+        <UserAveter
+          userName={UserData.user.name}
+          ImageSrc={UserData.user.image?.url}
+          AvaterSize={120}
+        />
+      </ImageWapper>
+
+      <Grid>
+
+        <FlexCardContent>
+          <Typography variant="h4">
+            {UserData.user.name}
+          </Typography>
+          {
+            UserData.user.id == currentUser.id ?
+            <>
+              <EditButton
+                onClick={()=> {history.push(`/user/${UserData.user?.name}/edit`)}}
+                variant="outlined" 
+              >
+                修正
+              </EditButton>
+            </>
+            :
+            null
+          }
+        </FlexCardContent>
+
+        <CardActions>
+          <Box>
+            投稿{UserData.posts.length}件
+          </Box>
+
+          <Box>
+            フォロワー0人
+          </Box>
+
+          <Box>
+            フォロー0人
+          </Box>
+        </CardActions>
+      </Grid>
+
+
+
+    </FlexBox>
+  )
+}
--- frontend/src/components/pages/UserProfile.tsx
+++ frontend/src/components/pages/UserProfile.tsx
@@ -1,31 +1,19 @@
 import { useContext, useEffect, useState } from "react";
-import { useHistory } from "react-router-dom";
-import styled from "styled-components";
 
 //material
-import { 
-  makeStyles, 
-  Theme,
-  CardActions,
-  CardContent,
-  Typography,
-  Grid,
-  Box,
-  Button,
-  LinearProgress
-} from "@material-ui/core";
-import Avatar from '@mui/material/Avatar';
-import SettingsIcon from '@material-ui/icons/Settings';
-
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
+import { LinearProgress } from "@material-ui/core";
 
 // useContext
 import { AuthContext } from "App";
 
-// components
+// template
 import { CommonLayout } from "components/templates/CommonLayout";
 import { LoadLayout } from "components/templates/LoadLayout";
+
+// organisms
+import { UserProfileItem } from "components/organisms/UserProfile/UserProfileItem";
+import { UserProfileSwichAction } from "components/organisms/UserProfile/UserProfileSwichAction";
+import { UserProfilePosts } from "components/organisms/UserProfile/UserProfilePosts";
 
 // api
 import { userShowData } from "apis/user";
@@ -33,47 +21,19 @@ import { userShowData } from "apis/user";
 // intarface
 import { UserData } from "interfaces/data/UserData";
 
-const useStyles = makeStyles((theme: Theme) => ({
-  root: {
-    display: "flex",
-  },
-  ImageGrid:{
-    padding: "10px"
-  },
-  CardContent: {
-    display: "flex"
-  },
-  EditButton: {
-    marginLeft: "15px"
-  },
-  ImageListItem: {
-    cursor: "pointer",
-    border: "1px solid #e4e4e4",
-  },
-  ActionButton: {
-    fontWeight: "bold",
-    color: "#9e9e9e",
-  }
-}))
-
-
 export const UserProfile = ({match}:any) => {
 
   const params = {name: match.params.userName}
 
-  const history = useHistory();
-
-  const classes = useStyles();
-
   const { currentUser } = useContext(AuthContext)
   
-  const [user, setUser] = useState<UserData>()
+  const [UserData, setUserData] = useState<UserData>()
 
   useEffect(() => {
     if (currentUser) {
       userShowData(params)
       .then((data) => {
-        setUser(data.user)
+        setUserData(data.user)
       })
       .catch((error) => {
         console.log(error)
@@ -81,146 +41,29 @@ export const UserProfile = ({match}:any) => {
     }
   },[currentUser])
 
-
   const [action, setAction] = useState<"ALL" | "LIKE">("ALL")
-
-  const handleAllPosts = (e: any) => {
-    if (action == "ALL") return
-    const TargetButton = e.currentTarget
-    TargetButton.id = "switch_active"
-    TargetButton.nextElementSibling.id = ""
-    setAction("ALL")
-  }
-
-  const handleLikePosts = (e: any) => {
-    if (action == "LIKE") return
-    const TargetButton = e.currentTarget
-    TargetButton.id = "switch_active"
-    TargetButton.previousElementSibling.id = ""
-    setAction("LIKE")
-  }
 
   return (
     <>
       {
-        user  ?
+        UserData && currentUser  ?
 
           <CommonLayout>
-            
-            <Box className={classes.root}>
 
-              <Grid className={classes.ImageGrid}>
+            <UserProfileItem
+              currentUser={currentUser}
+              UserData={UserData}
+            />
 
-                <Avatar
-                  alt={user.user.name}
-                  src={user.user.image?.url}
-                  sx={{ width: 120, height: 120 }}
-                />
+            <UserProfileSwichAction
+              action={action}
+              setAction={setAction}
+            />
 
-              </Grid>
-
-              <Grid >
-
-                <CardContent className={classes.CardContent}>
-                  <Typography variant="h4">
-                    {user.user.name}
-                  </Typography>
-                  {
-                    user.user?.id == currentUser?.id ?
-                    <>
-                      <Button
-                        onClick={()=> {history.push(`/user/${user.user?.name}/edit`)}}
-                        variant="outlined" 
-                        className={classes.EditButton}
-                      >
-                        修正
-                      </Button>
-                      <Button><SettingsIcon/></Button>
-                    </>
-                    :
-                    <></>
-                  }
-                </CardContent>
-
-                <CardActions>
-                  <Box>
-                    投稿{user.posts.length}件
-                  </Box>
-
-                  <Box>
-                    フォロワー0人
-                  </Box>
-
-                  <Box>
-                    フォロー0人
-                  </Box>
-                </CardActions>
-
-              </Grid>
-
-            </Box>
-
-            <Box
-              style={{
-                borderTop: "1px solid rgba(var(--b38,219,219,219),1)",
-                paddingTop: "10px",
-              }}
-            >
-              <Grid container justifyContent="center">
-              
-                <Button
-                  id="switch_active"
-                  className={classes.ActionButton}
-                  onClick={(e) => {handleAllPosts(e)}}
-                >
-                  投稿
-                </Button>
-
-                <Button
-                  className={classes.ActionButton}
-                  onClick={(e) => {handleLikePosts(e)}}
-                >
-                  お気に入り
-                </Button>
-
-              </Grid>
-
-            </Box>
-
-            <Box>
-
-              <ImageList
-                variant="quilted"
-                cols={3}
-              >
-                {
-                  action == "ALL" ?
-                  user.posts.map((post) => (
-                    <ImageListItem
-                      key={post.post.id}
-                      className={classes.ImageListItem}
-                      onClick={() => {history.push(`/post/${post.post.id}`)}}
-                      sx={{maxWidth: {xs: "120px", md: "300px"},maxHeight: {xs: "120px", md: "300px"}}}
-                    >
-                      <img src={post.photos[0].image?.url}/>
-                    </ImageListItem>
-                  ))
-                  :
-                  user.like_posts.map((post) => (
-                    <ImageListItem
-                      key={post.post.id}
-                      className={classes.ImageListItem}
-                      onClick={() => {history.push(`/post/${post.post.id}`)}}
-                      sx={{maxWidth: {xs: "120px", md: "300px"},maxHeight: {xs: "120px", md: "300px"}}}
-                    >
-                      <img src={post.photos[0].image?.url}/>
-                    </ImageListItem>
-                  ))
-                }
-
-              </ImageList>
-
-            </Box>
+            <UserProfilePosts
+              UserData={UserData}
+              action={action}
+            />
 
           </CommonLayout>
         :
