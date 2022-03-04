55e4886ee80b7ebffada19ae924af49ab1e44286
YutoHoshino <yutohoshino@Mac.local>
2022-03-05 01:14:36 +0900

    ヘッダーリファクタニング

--- frontend/src/components/organisms/Header/PrimaryHeader.tsx
+++ frontend/src/components/organisms/Header/PrimaryHeader.tsx
@@ -1,15 +1,15 @@
 import { useContext, useState } from "react";
-import { Link, useHistory } from "react-router-dom";
+import styled from "styled-components";
 
 // material
 import Box from '@mui/material/Box';
-import { styled, alpha } from '@mui/material/styles';
 import InputBase from '@mui/material/InputBase';
-import SearchIcon from '@mui/icons-material/Search';
 import Typography from '@mui/material/Typography';
-import { Avatar, AppBar, Toolbar, IconButton, makeStyles, Button } from "@material-ui/core"
+import { Avatar, AppBar, Button } from "@material-ui/core"
+import IconButton from '@mui/material/IconButton';
 
 // material icon
+import SearchIcon from '@mui/icons-material/Search';
 import CreateOutlinedIcon from '@mui/icons-material/CreateOutlined';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
@@ -17,93 +17,68 @@ import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
 // useContext
 import { AuthContext } from "App";
 
-// apis
+// Modal
+import { SearchModal } from "components/organisms/Modal/SearchModal";
 import { PostModal } from "components/organisms/Modal/PostModal";
-
-// compornent
 import { UserModal } from "components/organisms/Modal/UserModal";
 
-// material css
-const useStyles = makeStyles(
-  {
-    appBar: {
-      background: "white",
-      boxShadow: 'none',
-      borderBottom: "solid 1px #dbdbdb",
 
-    },
-    title: {
-      flexGrow: 1,
-      textDecoration: "none",
-      color: "inherit",
-    }
-  }
-);
 
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.25),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.35),
-  },
-  marginRight: theme.spacing(2),
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(3),
-    width: 'auto',
-  },
-}));
 
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  background: "#efefef",
-  borderRadius: "10px",
-  color: 'inherit',
-  '& .MuiInputBase-input': {
-    fontSize: '16px', 
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '30ch',
-    },
-  },
-}));
+const HeaderBar = styled(AppBar)`
+  background: white;
+  box-shadow: none;
+  border-bottom: solid 1px #dbdbdb;
+`
+const Toolbar = styled(Box)`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+const LogoText = styled(Typography)`
+  text-decoration: none;
+  font-weight: bold !important;
+`
+const SeachWapper = styled(Box)`
+  padding: 0 10px;
+  border-radius: 10px;
+  background: #efefef;
+  display: flex;
+  align-items: center;
+`
+const Input = styled(InputBase)`
+  font-size: 13px !important;
+  padding: 3px 5px;
+`
+const Search = styled(SearchIcon)`
+  color: rgb(142, 142, 142);
+`
+const ItemWapper = styled(Box)`
+  display: flex;
+`
 
 export const PrimaryHeader = () => {
 
   const { currentUser } = useContext(AuthContext)
 
-  const classes = useStyles();
-
-  const history = useHistory();
-
+  // Postモーダル
   const [isOpen, setIsOpen] = useState<boolean>(false)
+  const handlePost = (e: any) => {
+    e.preventDefault()
+    setIsOpen(true)
+  }
+  
 
-  // PostMenuモーダル表示
+  // Userメニューモーダル
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const isPostModal = Boolean(anchorEl);
   const handleMobileMenuOpen = (event: any) => {
     setAnchorEl(event.currentTarget);
   }
 
-  // 投稿モーダル表示
-  const handlePost = (e: any) => {
-    e.preventDefault()
-    setIsOpen(true)
-  }
-  
+
+  // 検索ロジック
+  const [SearchText, setSearchText] = useState<string>()
 
   return (
     <>
@@ -111,48 +86,28 @@ export const PrimaryHeader = () => {
         currentUser ? 
 
 
-          <AppBar
-            color="default"
-            className={classes.appBar}
-          >
-            <Box
-              sx={{ padding: {xs: '0', md: '0 200px'} }}
-            >
-              <Toolbar>
-                <Typography
-                  component={Link}
-                  to="/"
-                  variant="h6"
-                  className={classes.title}
-                  style={{fontWeight: "bold", }}
+          <HeaderBar color="default">
+
+            <Toolbar sx={{ padding: {xs: '10px', md: '0 320px'} }}>
+
+                <LogoText>photographar</LogoText>
+
+
+                <SeachWapper
+                  sx={{ display: {xs: 'none', md: 'flex'} }}
                 >
-                  photographar
-                </Typography>
+                  <Search/>
+                  <Input
+                    placeholder="検索..."
+                    onChange={e => setSearchText(e.target.value)}
+                  />
 
-                <Search
-                  sx={{ 
-                    display: {xs: 'none', md: 'block' },
-                    fontSize: "16px"
-                  }}
-                >
-                  <SearchIconWrapper>
-                    <SearchIcon style={{zIndex:"2", color:"#9c9c9c"}}/>
-                  </SearchIconWrapper>
-                  <StyledInputBase
-                    placeholder="検索"
-                    inputProps={{ 'aria-label': 'search' }}
-                  />  
-                </Search>
-
-                <Box sx={{ flexGrow: 1 }} />
+                </SeachWapper>
 
 
-                {/* アイコン */}
-                <Box
-                  sx={{display: "flex"}}
-                >
+                <ItemWapper>
+
                   <IconButton
-
                     onClick={handlePost}
                   >
                     <CreateOutlinedIcon/>
@@ -168,8 +123,6 @@ export const PrimaryHeader = () => {
                     <NotificationsNoneIcon/>
                   </IconButton>
 
-
-                  {/* ユーザーアイコン */}
                   <IconButton
                     onClick={handleMobileMenuOpen} 
                   >
@@ -179,11 +132,10 @@ export const PrimaryHeader = () => {
                     />
                   </IconButton>
                   
-                </Box>
+                </ItemWapper>
 
-              </Toolbar>
-            </Box>
-          </AppBar>
+            </Toolbar>
+          </HeaderBar>
 
         :
         <></>
