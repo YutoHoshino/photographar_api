ac82fb7bf784f1cf2557cea65ae71913ed7f0c60
YutoHoshino <yutohoshino@Mac.local>
2022-03-15 16:01:31 +0900

    loginのlogを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::API
   def login(user)
     remember_token = User.new_remember_token
     session[:user_remember_token] = remember_token
+    p "loginここにsessionを表示"
+    p session[:user_remember_token]
     user.update!(remember_token: User.encrypt(remember_token))
     @current_user = user
   end
