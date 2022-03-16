5f7f52ec9ce8df4a7ab7410b46a6aff9f081db8d
YutoHoshino <yutohoshino@Mac.local>
2022-03-16 14:03:19 +0900

    session_store修正

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: '_auth-app-api'
+if Rails.env === 'production'
+  Rails.application.config.session_store :cookie_store, key: '_auth-app-api', domain: 'photographar-react-frontend.herokuapp.com'
+else
+  Rails.application.config.session_store :cookie_store, key: '_auth-app-api'
+end
