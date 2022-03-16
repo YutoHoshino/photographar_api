5a549b16df80a9ac2d7fef925f4b8d969b1ccd3f
YutoHoshino <yutohoshino@Mac.local>
2022-03-16 14:56:09 +0900

    修正

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,5 @@
 if Rails.env === 'production'
-  Rails.application.config.session_store :redis_store, {
-    servers: ENV['REDIS_TLS_URL'],
-    expire_after: 90.minutes # 保存期間
-  }
+  Rails.application.config.session_store :cookie_store, key: '_auth-app-api'
 else
   Rails.application.config.session_store :cookie_store, key: '_auth-app-api'
 end
