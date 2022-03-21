0457f5ef7be1474670f60995a21dcb0acb751f91
YutoHoshino <yutohoshino@Mac.local>
2022-03-21 13:59:05 +0900

    修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ Rails.application.configure do
   config.log_tags = [ :request_id ]
 
   # Use a different cache store in production.
-  config.cache_store = :redis_store, servers: ENV["REDIS_URL"], expire_after: 60.minutes
+  config.cache_store = :redis_store
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
@@ -103,5 +103,5 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.session_store :redis_store, servers: ENV["REDIS_URL"], expire_after: 60.minutes
+  config.session_store = :redis_store, servers: ENV["REDIS_URL"], expire_after: 60.minutes
 end
