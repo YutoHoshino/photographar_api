754c015c952201f57c705ab1b57a9a5a253344a3
YutoHoshino <yutohoshino@Mac.local>
2022-04-01 18:25:40 +0900

    修正

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,1 @@
-# if Rails.env === 'production'
-#   Rails.application.config.session_store :cookie_store, key: '_auth-app-api', domain: 'photographar-front.herokuapp.com'
-# else
-#   Rails.application.config.session_store :cookie_store, key: '_auth-app-api'
-# end
-
-# Rails.application.config.session_store :cookie_store, key: '_auth_api', :domain => :all
+Rails.application.config.session_store :redis_store, servers: Rails.application.config.session_store_servers, expire_in: BestTeacher::Application.config.session_expires_in
