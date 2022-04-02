207626fc0ce1cd8c4ee72c2d355b994e0ce254e5
YutoHoshino <yutohoshino@Mac.local>
2022-04-02 16:02:36 +0900

    redis

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,21 +4,13 @@ class ApplicationController < ActionController::API
   def login(user)
     remember_token = User.new_remember_token
     session[:user_remember_token] = remember_token
-    p "loginここにsessionを表示"
-    p session[:user_remember_token]
     user.update!(remember_token: User.encrypt(remember_token))
     @current_user = user
   end
 
   def current_user
-    p "ここにsessionを表示"
-    p session[:user_remember_token]
     remember_token = User.encrypt(session[:user_remember_token])
     @current_user ||= User.find_by(remember_token: remember_token)
   end
-
-  def time_wait
-    sleep 1
-  end
-
+  
 end
--- config/initializers/session_store.rb
+++
@@ -1,1 +0,0 @@
-Rails.application.config.session_store :redis_store, servers: Rails.application.config.session_store_servers, expire_in: BestTeacher::Application.config.session_expires_in
