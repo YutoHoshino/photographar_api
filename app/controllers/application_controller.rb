6a0fe8000971e243d1e0627611a8dc4a31b56df8
YutoHoshino <yutohoshino@Mac.local>
2022-04-02 17:29:58 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,19 @@ class ApplicationController < ActionController::API
   def login(user)
     remember_token = User.new_remember_token
     session[:user_remember_token] = remember_token
+    p 'ログイン セッションオプション'
+    p session.options
+    p 'ログイン 情報'
+    p session[:user_remember_token]
     user.update!(remember_token: User.encrypt(remember_token))
     @current_user = user
   end
 
   def current_user
+    p 'カレントユーザー取得 セッションオプション'
+    p session.options
+    p 'カレントユーザー取得 情報'
+    p session[:user_remember_token]
     remember_token = User.encrypt(session[:user_remember_token])
     @current_user ||= User.find_by(remember_token: remember_token)
   end
