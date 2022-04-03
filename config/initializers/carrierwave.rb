4350db4c3d314b64c1d060bad3d6b13582bef8d0
YutoHoshino <yutohoshino@Mac.local>
2022-04-03 15:13:40 +0900

    修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   if Rails.env === 'production'
-    config.asset_host = "http://localhost:3001"
-  else
     config.asset_host = "https://photographar-api.herokuapp.com"
+  else
+    config.asset_host = "http://localhost:3001"
   end
   config.storage = :file
   config.cache_storage = :file
