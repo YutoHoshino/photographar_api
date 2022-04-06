3cd1a43fb58685d569916c74d2c7211204e7983a
YutoHoshino <yutohoshino@Mac.local>
2022-04-07 02:03:29 +0900

    修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,6 +7,8 @@ CarrierWave.configure do |config|
     config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'photographer-bucket'
+    config.asset_host = "https://s3.ap-northeast-1.amazonaws.com/photographer-bucket"
+
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
