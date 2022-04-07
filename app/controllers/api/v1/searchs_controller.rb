1b1c72af894bea13100a0c013b30867069e61898
YutoHoshino <yutohoshino@Mac.local>
2022-04-07 23:18:30 +0900

    検索で自分が出てしまう問題解消

--- app/controllers/api/v1/searchs_controller.rb
+++ app/controllers/api/v1/searchs_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::SearchsController < ApplicationController
 
   def search
-    users = User.search(params[:keyword])
+    users = User.search(params[:keyword]).where.not(id: current_user.id)
     if users.present?
       render json: { users: users }, status: :ok
     else
